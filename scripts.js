a615f34972a1c5682d26742502d58fddab2e59cf
Did-Vlad <steam.gaming.prvt@gmail.com>
2024-07-17 10:56:24 +0300

    add scripts for butons

--- scripts.js
+++ scripts.js
@@ -1,1 +1,41 @@
-
+document.addEventListener('DOMContentLoaded', function() {
+  var button = document.getElementById('film1-button');
+  button.addEventListener('click', function() {
+      window.location.href = 'movie_pages/movie1_page.html'; 
+  });
+});
+document.addEventListener('DOMContentLoaded', function() {
+  var button = document.getElementById('film2-button');
+  button.addEventListener('click', function() {
+      window.location.href = 'movie_pages/movie2_page.html'; 
+  });
+});
+document.addEventListener('DOMContentLoaded', function() {
+  var button = document.getElementById('film3-button');
+  button.addEventListener('click', function() {
+      window.location.href = 'movie_pages/movie3_page.html'; 
+  });
+});
+document.addEventListener('DOMContentLoaded', function() {
+  var button = document.getElementById('film4-button');
+  button.addEventListener('click', function() {
+      window.location.href = 'movie_pages/movie4_page.html';
+  });
+});
+document.addEventListener('DOMContentLoaded', function() {
+  var button = document.getElementById('film5-button');
+  button.addEventListener('click', function() {
+      window.location.href = 'movie_pages/movie5_page.html';
+  });
+});
+document.addEventListener('DOMContentLoaded', function() {
+  var button = document.getElementById('film6-button');
+  button.addEventListener('click', function() {
+      window.location.href = 'movie_pages/movie6_page.html';
+  });
+});
+document.addEventListener('DOMContentLoaded', function(){ 
+  var button = document.getElementById('film7-button');
+  button.addEventListener('click', function() {
+      window.location.href = 'movie_pages/movie7_page.html'; // Замість 'anotherPage.html' вкажіть шлях до потрібної сторінки
+  })};
